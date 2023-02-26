fb5d7aec016c09a922e1ded0aad89a034c3d161c
Luis Almeida <lferreira646@gmail.com>
2023-02-26 17:25:50 -0300

    Fix: timestamp without time zone, offer, notification and enrollment

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -15,9 +15,9 @@ public class Enrollment implements Serializable {
 
     @EmbeddedId
     private EnrollmentPK id = new EnrollmentPK();
-    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant enrollMoment;
-    @Column(columnDefinition = "TIMESTAMP  WITH TIME ZONE")
+    @Column(columnDefinition = "TIMESTAMP  WITHOUT TIME ZONE")
     private Instant refundMoment;
     private boolean available;
     private boolean onlyUpdate;
