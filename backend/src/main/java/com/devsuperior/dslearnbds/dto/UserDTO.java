44a9f99cfe32c6f672d18a672c93d854cebe7bd1
Luis Almeida <lferreira646@gmail.com>
2023-03-02 17:19:38 -0300

    fix: properties, and User dto and resource

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/dto/UserDTO.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dslearnbds.dto;
+
+import com.devsuperior.dslearnbds.entities.User;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+
+    private Long  id;
+    private String name;
+    private String email;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public UserDTO(User entity) {
+        id = entity.getId();
+        name = entity.getName();
+        email = entity.getEmail();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
